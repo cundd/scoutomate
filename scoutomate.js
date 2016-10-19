8538e863b7f2611e8503c0c827ed762fabb5f8be
Daniel Corn <d@cundd.net>
2016-10-19 17:24:40 +0200

    Formatted code

--- scoutomate.js
+++ scoutomate.js
@@ -2,53 +2,53 @@
  * Created by COD on 30.06.14.
  */
 (function () {
-	var scoutomateInstance;
+    var scoutomateInstance;
 
-	/**
-	 * Creates a new instance
-	 *
-	 * @param {Object} data Object with keys to define selectors and values or actions
-	 * @param {String} [root] Selector of the root element
-	 * @param {Object} [options]
-	 * @constructor
-	 */
-	var Scoutomate = window.Scoutomate = function (data, root, options) {
-		var ef = function () {
-		};
-		this.data = data;
-		this.root = root ? document.querySelector(root) : document.querySelector('body');
-		this.options = options || {
-			'autofill': true
-		};
+    /**
+     * Creates a new instance
+     *
+     * @param {Object} data Object with keys to define selectors and values or actions
+     * @param {String} [root] Selector of the root element
+     * @param {Object} [options]
+     * @constructor
+     */
+    var Scoutomate = window.Scoutomate = function (data, root, options) {
+        var ef = function () {
+        };
+        this.data = data;
+        this.root = root ? document.querySelector(root) : document.querySelector('body');
+        this.options = options || {
+                'autofill': true
+            };
 
-		this.console = window.console || {
-			log:   ef,
-			debug: ef,
-			info:  ef
-		};
+        this.console = window.console || {
+                log: ef,
+                debug: ef,
+                info: ef
+            };
 
-		if (data && this.options.autofill) {
-			this.fill();
-		}
-	};
+        if (data && this.options.autofill) {
+            this.fill();
+        }
+    };
 
-	Scoutomate.Actions = {};
+    Scoutomate.Actions = {};
 
-	/**
-	 * Constant to perform a click
-	 *
-	 * @type {string}
-	 */
-	Scoutomate.Actions.click = 'Scoutomate.Actions.click';
+    /**
+     * Constant to perform a click
+     *
+     * @type {string}
+     */
+    Scoutomate.Actions.click = 'Scoutomate.Actions.click';
 
-	/**
-	 * Constant to select an option
-	 *
-	 * @type {string}
-	 */
-	Scoutomate.Actions.select = 'Scoutomate.Actions.select';
+    /**
+     * Constant to select an option
+     *
+     * @type {string}
+     */
+    Scoutomate.Actions.select = 'Scoutomate.Actions.select';
 
-	Scoutomate.prototype = {
+    Scoutomate.prototype = {
         /**
          * Select a value in a select box
          *
@@ -89,69 +89,69 @@
         },
 
         /**
-		 * Fills the given field or performs the action
-		 *
-		 * @param {string} selector
-		 * @param {*} valueOrAction
-		 * @returns {NodeList}
-		 */
-		handle: function (selector, valueOrAction) {
-			var _this = this;
-			var _console = this.console;
-			_console.debug('Looking for "' + selector + '"');
-			var domNodes = this.root.querySelectorAll(selector);
-			_console.debug(domNodes);
+         * Fills the given field or performs the action
+         *
+         * @param {string} selector
+         * @param {*} valueOrAction
+         * @returns {NodeList}
+         */
+        handle: function (selector, valueOrAction) {
+            var _this = this;
+            var _console = this.console;
+            _console.debug('Looking for "' + selector + '"');
+            var domNodes = this.root.querySelectorAll(selector);
+            _console.debug(domNodes);
 
-			Array.prototype.forEach.call(domNodes, function (element) {
-				_console.debug(valueOrAction);
-				if (valueOrAction === Scoutomate.Actions.click) {
-					_this.click(element);
-				} else if (valueOrAction === Scoutomate.Actions.select) {
-					_this.select(element);
-				} else {
-					_this.fillField(element, valueOrAction);
-				}
-			});
-		},
+            Array.prototype.forEach.call(domNodes, function (element) {
+                _console.debug(valueOrAction);
+                if (valueOrAction === Scoutomate.Actions.click) {
+                    _this.click(element);
+                } else if (valueOrAction === Scoutomate.Actions.select) {
+                    _this.select(element);
+                } else {
+                    _this.fillField(element, valueOrAction);
+                }
+            });
+        },
 
-		/**
-		 * Loops through each property of data and takes the keys to search the DOM for matching elements which will
-		 * then be filled with the property value or a special action will be performed if the value tells to
-		 */
-		fill: function () {
-			var _data = this.data,
-				name;
+        /**
+         * Loops through each property of data and takes the keys to search the DOM for matching elements which will
+         * then be filled with the property value or a special action will be performed if the value tells to
+         */
+        fill: function () {
+            var _data = this.data,
+                name;
 
-			for (name in _data) {
-				if (_data.hasOwnProperty(name)) {
-					this.handle(name,_data[name]);
-				}
-			}
-		},
+            for (name in _data) {
+                if (_data.hasOwnProperty(name)) {
+                    this.handle(name, _data[name]);
+                }
+            }
+        },
 
-		/**
-		 * Triggers the event with the given name on the given node
-		 *
-		 * @param {HTMLElement} node
-		 * @param {String} eventName
-		 * @param {*} [data] currently unused
-		 */
-		triggerEvent: function (node, eventName, data) {
-			if (eventName === 'click') {
-				this.console.log(node);
-				node.click();
-				return;
-			}
-			var event = document.createEvent('HTMLEvents');
-			event.initEvent(eventName, true, false);
-			node.dispatchEvent(event);
-		}
-	};
+        /**
+         * Triggers the event with the given name on the given node
+         *
+         * @param {HTMLElement} node
+         * @param {String} eventName
+         * @param {*} [data] currently unused
+         */
+        triggerEvent: function (node, eventName, data) {
+            if (eventName === 'click') {
+                this.console.log(node);
+                node.click();
+                return;
+            }
+            var event = document.createEvent('HTMLEvents');
+            event.initEvent(eventName, true, false);
+            node.dispatchEvent(event);
+        }
+    };
 
-	if (window.ScoutomateData) {
-		scoutomateInstance = new Scoutomate(window.ScoutomateData);
-	} else {
-		scoutomateInstance = new Scoutomate();
-	}
-	return scoutomateInstance;
+    if (window.ScoutomateData) {
+        scoutomateInstance = new Scoutomate(window.ScoutomateData);
+    } else {
+        scoutomateInstance = new Scoutomate();
+    }
+    return scoutomateInstance;
 })();
